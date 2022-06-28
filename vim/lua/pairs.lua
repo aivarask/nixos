cdb1a287cdc39bcc513134dcbcc87b8a2e92735c
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2022-06-28 20:37:52 +0300

    ~ nvim-autopairs config

--- vim/lua/pairs.lua
+++ vim/lua/pairs.lua
@@ -1,25 +1,25 @@
 local npairs = require("nvim-autopairs")
 local cmp = require("cmp")
 npairs.setup({
-  -- https://github.com/windwp/nvim-autopairs
-  disable_filetype = { "TelescopePrompt" },
-  enable_check_bracket_line = false,
-  ignored_next_char = "[%w%.]",
-  fast_wrap = {}, -- https://github.com/windwp/nvim-autopairs#fastwrap
-  check_ts = true,
-  ts_config = {
-    lua = { "string" }, -- it will not add a pair on that treesitter node
-    javascript = { "template_string" },
-    java = false, -- don't check treesitter on java
-  },
+	-- https://github.com/windwp/nvim-autopairs
+	disable_filetype = { "TelescopePrompt" },
+	enable_check_bracket_line = false,
+	ignored_next_char = "[%w%.]",
+	fast_wrap = {}, -- https://github.com/windwp/nvim-autopairs#fastwrap
+	check_ts = true,
+	ts_config = {
+		lua = { "string" }, -- it will not add a pair on that treesitter node
+		-- javascript = { "template_string" },
+		java = false, -- don't check treesitter on java
+	},
 })
 
 local ts_conds = require("nvim-autopairs.ts-conds")
 local Rule = require("nvim-autopairs.rule")
 -- press % => %% only while inside a comment or string
 npairs.add_rules({
-  Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node({ "string", "comment" })),
-  Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({ "function" })),
+	Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node({ "string", "comment" })),
+	Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({ "function" })),
 })
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
