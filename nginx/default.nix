6860774afa9a906e4b794550298a6d404e06a659
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2022-07-23 09:51:40 +0300

    ~ nginx

--- nginx/default.nix
+++ nginx/default.nix
@@ -1,22 +1,22 @@
 { config, pkgs, lib, ... }: {
-  systemd.services.nginx.serviceConfig.ReadWritePaths = [
-    "/var/spool/nginx/logs/"
-    "/var/www"
-  ];
-  services.nginx = {
-    enable = true;
-    virtualHosts = {
-      "localhost" = {
-        forceSSL = false;
-        enableACME = false;
-        # All serverAliases will be added as extra domain names on the certificate.
-        # serverAliases = [ "bar.example.com" ];
-        locations."/" = {
-          root = "/var/www";
-        };
-      };
-    };
-  };
+  # systemd.services.nginx.serviceConfig.ReadWritePaths = [
+  #   "/var/spool/nginx/logs/"
+  #   "/var/www"
+  # ];
+  # services.nginx = {
+  #   enable = true;
+  #   virtualHosts = {
+  #     "localhost" = {
+  #       forceSSL = false;
+  #       enableACME = false;
+  #       # All serverAliases will be added as extra domain names on the certificate.
+  #       # serverAliases = [ "bar.example.com" ];
+  #       locations."/" = {
+  #         root = "/var/www";
+  #       };
+  #     };
+  #   };
+  # };
 
   # We can also add a different vhost and reuse the same certificate
   # but we have to append extraDomainNames manually.
