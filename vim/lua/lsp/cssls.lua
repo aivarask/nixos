e3ede62b8aaff6bc20347add68c4691bc6977ab0
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2022-08-17 09:45:31 +0300

    FIXME: cssls still not autostarts

--- vim/lua/lsp/cssls.lua
+++ vim/lua/lsp/cssls.lua
@@ -3,17 +3,17 @@ require("lspconfig").cssls.setup({
 	capabilities = require("capabilities"),
 	on_attach = require("on_attach"),
 	flags = require("flags"),
-	init_options = {
-		-- https://github.com/MaskRay/ccls/wiki/Customization#initialization-options
-		cache = {
-			directory = "~/.cache/ccls-cache",
-		},
-		compilationDatabaseDirectory = "build",
-		index = {
-			threads = 0,
-		},
-		clang = {
-			excludeArgs = { "-frounding-math" },
-		},
-	},
+	-- init_options = {
+	-- 	-- https://github.com/MaskRay/ccls/wiki/Customization#initialization-options
+	-- 	cache = {
+	-- 		directory = "~/.cache/ccls-cache",
+	-- 	},
+	-- 	compilationDatabaseDirectory = "build",
+	-- 	index = {
+	-- 		threads = 0,
+	-- 	},
+	-- 	clang = {
+	-- 		excludeArgs = { "-frounding-math" },
+	-- 	},
+	-- },
 })
