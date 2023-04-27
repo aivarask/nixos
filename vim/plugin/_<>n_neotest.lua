d2ce245b5afb1b19956c8f2f8eeade51429676fc
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2023-04-27 15:17:33 +0300

    neotest adjust

--- vim/plugin/_<>n_neotest.lua
+++ vim/plugin/_<>n_neotest.lua
@@ -1,5 +1,20 @@
 local neotest = require('neotest')
 
+local vitestAdapter = require('neotest-vitest')
+---@diagnostic disable-next-line: duplicate-set-field
+vitestAdapter['filter_dir'] = function(name)
+  return (name ~= 'node_modules' and name ~= 'tests' and name ~= '_tests')
+end
+
+local playwrightAdapter = require('neotest-playwright').adapter({})
+playwrightAdapter.filter_dir = function(name)
+  return (name ~= 'node_modules' and name ~= 'src' and name ~= '_tests')
+end
+
+local plenaryAdapter = require('neotest-plenary').setup({
+  min_init = '/etc/nixos/vim/spec/test_init.lua',
+})
+
 -- https://github.com/nvim-neotest/neotest
 -- https://github.com/nvim-neotest/neotest/blob/master/lua/neotest/config/init.lua#L131
 require('neotest').setup({
@@ -15,47 +30,9 @@ require('neotest').setup({
     open = false,
   },
   adapters = {
-    -- require('neotest-plenary').setup({
-    --   -- https://github.com/nvim-neotest/neotest-plenary
-    --   min_init = '/etc/nixos/vim/spec/test_init.lua',
-    -- }),
-    -- require('neotest-vitest'),
-    -- https://github.com/thenbe/neotest-playwright#configuration-1
-    require('neotest-playwright').adapter({
-      options = {
-        persist_project_selection = false,
-
-        enable_dynamic_test_discovery = false,
-
-        preset = 'none', -- "none" | "headed" | "debug"
-
-        -- get_playwright_binary = function()
-        --    return vim.loop.cwd() + "/node_modules/.bin/playwright"
-        -- end,
-
-        -- get_playwright_config = function()
-        --    return vim.loop.cwd() + "/playwright.config.ts"
-        -- end,
-
-        -- get_cwd = function()
-        --    return vim.loop.cwd()
-        -- end,
-
-        -- env = { },
-
-        -- Extra args to always pass to playwright.
-        -- These are merged with any extra_arg passed
-        -- to neotest's run command.
-        -- extra_args = { },
-
-        -- Filter directories when searching for test files,
-        -- useful in large projects (see performance notes).
-        filter_dir = function(name, rel_path, root)
-          print(name)
-          return name ~= 'src'
-        end,
-      },
-    }),
+    vitestAdapter,
+    playwrightAdapter,
+    plenaryAdapter,
   },
 })
 
@@ -73,18 +50,37 @@ end
 
 wkr({
   name = 'Neotest',
-  r = { neotest.run.run, 'run' },
-  a = { neotest.run.attach, 'attach' },
-  l = { neotest.run.run_last, 'run_last' },
+  r = { neotest.run.run, 'run.run' },
+  a = { neotest.run.attach, 'run.attach' },
+  l = { neotest.run.run_last, 'run.run_last' },
   -- s = { neotest.run.stop, 'stop' },
-  s = { neotest.summary.toggle, 'summary' },
-  o = { neotest.output_panel.toggle, 'output_panel' },
-  S = { neotest.run.suite, 'suite' },
-  b = { neotest.run.buffer, 'buffer' },
-  d = { neotest.run.dap, 'dap' },
+  s = { neotest.summary.toggle, 'summary.toggle' },
+  o = { neotest.output_panel.toggle, 'output_panel.toggle' },
+  S = { neotest.run.suite, 'run.suite' },
+  b = { neotest.run.buffer, 'run.buffer' },
+  d = { neotest.run.dap, 'run.dap' },
   c = { [[:!busted<cr>]], '!busted' },
 }, { prefix = '<leader>n' })
 wkr({
-  ['<leader>]'] = { require('neotest').summary.toggle, 'summary' },
-  ['<leader>}'] = { require('neotest').output_panel.toggle, 'output_panel' },
+  ['<leader>]'] = { require('neotest').summary.toggle, 'summary.toggle' },
+  ['<leader>}'] = { require('neotest').output_panel.toggle, 'output_panel.toggle' },
 })
+-- mappings = {
+--   attach = "a",
+--   clear_marked = "M",
+--   clear_target = "T",
+--   debug = "d",
+--   debug_marked = "D",
+--   expand = { "<CR>", "<2-LeftMouse>" },
+--   expand_all = "e",
+--   jumpto = "i",
+--   mark = "m",
+--   next_failed = "J",
+--   output = "o",
+--   prev_failed = "K",
+--   run = "r",
+--   run_marked = "R",
+--   short = "O",
+--   stop = "u",
+--   target = "t"
+-- },
