26b67603bfce71db8ae82bae3bb4ab25d0c66f05
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2023-05-14 09:23:14 +0300

    home-cursor default

--- home/home-cursor.nix
+++ home/home-cursor.nix
@@ -1,9 +1,13 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   # home.file.".icons/default".source = "${pkgs.vanilla-dmz}/share/icons/Vanilla-DMZ";
   home.pointerCursor = {
     name = "Vanilla-DMZ";
     package = pkgs.vanilla-dmz;
-    size = 64;
+    size = lib.mkDefault 64;
     x11 = {
       enable = true;
       # defaultCursor = "left_ptr";
