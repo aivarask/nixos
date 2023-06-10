ea819ef53576cc7f3ac95292a289f91f0c40a44b
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2023-06-11 02:47:25 +0300

    nested dir screen

--- modules/services/scheduling/cron.nix
+++ modules/services/scheduling/cron.nix
@@ -2,12 +2,6 @@ _: {
   services.cron = {
     enable = false;
     systemCronJobs = [
-      "@reboot root . /etc/profile; dig +short myip.opendns.com @resolver1.opendns.com > /tmp/ip"
-      "@hourly root . /etc/profile; dig +short myip.opendns.com @resolver1.opendns.com > /tmp/ip"
-      "@reboot root . /etc/profile; curl -s wttr.in/Vilnius?format=4 > /tmp/wttr"
-      "*/30 * * * * root . /etc/profile; curl -s wttr.in/Vilnius?format=4 > /tmp/wttr"
-      "@reboot root DISPLAY=:0 feh --bg-fill --randomize /etc/nixos/misc/wallpapers/abstract/*"
-      "*/30 * * * * root DISPLAY=:0 feh --bg-fill --randomize /etc/nixos/misc/wallpapers/abstract/*"
     ];
   };
 }
