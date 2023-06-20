0a8440903abdf02563198f176e616372e8c73d82
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2023-06-20 19:48:34 +0300

    wallpaper logic split

--- home/files.nix
+++ home/files.nix
@@ -1,5 +1,5 @@
 _: {
   home.file.".xinitrc".text = builtins.readFile ./files/xinitrc;
-  home.file.".wallpaper/super".source = ../images/wallpaper/super;
+  home.file.".wallpaper".source = ../images/wallpaper/super;
   home.file.".config/tilda/config_0".text = builtins.readFile ./files/tilda;
 }
