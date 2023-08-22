e4b8ae40ee1c0eb45e71e7caa6420ea9e34f759a
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2023-08-22 06:36:42 +0300

    mv redshift to sys module

--- modules/services/x11/redshift.nix
+++ modules/services/x11/redshift.nix
@@ -1,12 +1,14 @@
-_: {
+{lib, ...}: {
+  location = {
+    provider = lib.mkDefault "geoclue2"; # manual
+    latitude = 54.0;
+    longitude = 25.0;
+  };
   services.redshift = {
     enable = true;
     temperature = {
       day = 6500; # 5500
       night = 4500; # 3700
     };
-    latitude = 54.0;
-    longitude = 25.0;
-    provider = "manual";
   };
 }
