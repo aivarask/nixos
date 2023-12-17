b0431b92b8be982ac8597cbbb12d9d332c533527
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2023-12-17 06:47:48 +0200

    up

--- modules/config/system-environment.nix
+++ modules/config/system-environment.nix
@@ -1,13 +1,22 @@
 {pkgs, ...}: {
   environment = {
-    sessionVariables = {
+    sessionVariables = rec {
+      # https://nixos.wiki/wiki/Environment_variables
+      XDG_CACHE_HOME = "$HOME/.cache";
+      XDG_CONFIG_HOME = "$HOME/.config";
+      XDG_DATA_HOME = "$HOME/.local/share";
+      XDG_STATE_HOME = "$HOME/.local/state";
+
+      XDG_BIN_HOME = "$HOME/.local/bin";
       PATH = [
+        "${XDG_BIN_HOME}"
         "/etc/nixos/bin"
         "$HOME/.pnpm-global"
         "$HOME/.node_modules/bin"
         "$HOME/.config/composer/vendor/bin"
       ];
-      FOO = "environment.sessionVariables";
+
+      # FOO = "environment.sessionVariables";
       # PLAYWRIGHT
       # PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "1";
       PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = "1";
