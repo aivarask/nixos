458768ca6c5a261b20f41efa8d7c99cd325a365d
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2024-02-08 19:22:29 +0200

    telescope trouble

--- plugin/_telescope.lua
+++ plugin/_telescope.lua
@@ -6,40 +6,30 @@ require('telescope').setup({
     },
   },
 })
-
--- https://github.com/nvim-telescope/telescope-fzf-native.nvim
 require('telescope').load_extension('fzf')
 require('telescope').load_extension('dap')
 
 local find_files = require('telescope.builtin').find_files
 
 wkr({
-    name = 'Telescope find_files',
-    e = { [[:Telescope find_files<CR>]], '.' },
-  },
-  { prefix = '<leader>e' })
-
-wkr({
   name = 'Telescope',
-  n = { function() find_files({ cwd = '/etc/nixos' }) end, 'find_files /etc/nixos' },
-  s = { function() find_files({ cwd = '/etc/nixos/vim/snippets' }) end, 'find_files snippets' },
   a = { [[:Telescope autocommands<CR>]], 'autocommands' },
-  p = { [[:Telescope ak paths<CR>]], 'ak paths' },
-  r = { [[:Telescope registers<CR>]], 'registers' },
-  c = { [[:Telescope commands<cr> ]], 'commands' },
-  t = { [[:Telescope<CR>]], 'Telescope' },
   b = { [[:Telescope buffers <CR>]], 'buffers' },
-  k = { [[:Telescope keymaps<CR>]], 'keymaps' },
-  h = { [[:Telescope help_tags<CR>]], 'helptags' },
+  c = { [[:Telescope commands<cr> ]], 'commands' },
+  e = { [[:Telescope symbols<cr> ]], 'emoji symbols' },
   f = { [[:Telescope find_files<CR>]], 'find_files' },
   g = { [[:Telescope live_grep<CR>]], 'live_grep' },
+  h = { [[:Telescope help_tags<CR>]], 'helptags' },
+  k = { [[:Telescope keymaps<CR>]], 'keymaps' },
+  n = { function() find_files({ cwd = '/etc/nixos' }) end, 'find /etc/nixos' },
+  p = { [[:Telescope ak paths<CR>]], 'ak paths' },
+  r = { [[:Telescope registers<CR>]], 'registers' },
+  s = { function() find_files({ cwd = '/etc/nixos/vim/snippets' }) end, 'find snippets' },
+  t = { [[:Telescope<CR>]], 'Telescope' },
   l = {
     name = 'LSP',
     d = { [[:Telescope lsp_document_symbols<CR>]], 'lsp_document_symbols' },
     w = { [[:Telescope lsp_workspace_symbols<CR>]], 'lsp_workspace_symbols' },
-    x = {
-      [[:Telescope lsp_dynamic_workspace_symbols<CR>]],
-      'lsp_dynamic_workspace_symbols',
-    },
+    x = { [[:Telescope lsp_dynamic_workspace_symbols<CR>]], 'lsp_dynamic_workspace_symbols' },
   },
 }, { prefix = '<leader>t' })
