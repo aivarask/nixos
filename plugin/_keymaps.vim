edc63a3a88399c55708d9927869ee67c1a80632f
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2024-02-08 19:23:55 +0200

    three

---
+++ plugin/_keymaps.vim
@@ -0,0 +1,107 @@
+" nnoremap <leader>lf :LfCurrentDirectory<CR>
+" nnoremap <leader>lg :LazyGit<CR>
+" nnoremap <leader>lt :LfNewTab<CR>
+" nnoremap <leader>lw :LfWorkingDirectory<CR>
+
+nnoremap <C-s> :silent w<CR>
+inoremap <silent><C-s> <C-o>:silent w<CR>
+
+" digraph
+inoremap <C-z> <C-k>
+nmap k gk
+nnoremap <C-j> :m .+1<CR>==
+nnoremap <C-k> :m .-2<CR>==
+inoremap <C-j> <Esc>:m .+1<CR>==gi
+inoremap <C-k> <Esc>:m .-2<CR>==gi
+vnoremap <C-j> :m '>+1<CR>gv=gv
+vnoremap <C-k> :m '<-2<CR>gv=gv
+
+nnoremap <silent> <Plug>(Save) :silent w<cr>
+map <C-s> <Plug>(Save)
+inoremap <C-s> <C-o><C-s>
+
+" #alt
+" <M-[integer]> ./keymaps/_.lua
+" <M-e> https://github.com/windwp/nvim-autopairs#fastwrap 
+" <M-s> switch-nvim
+" #undo #alt
+inoremap <Space> <C-G>u<Space>
+inoremap <M-u> <C-O>u
+inoremap <M-r> <C-O><C-r>
+inoremap <M-w> <C-O>w
+inoremap <M-b> <C-O>b
+" #comment #alt
+nmap <M-c> gc$
+vmap <M-c> gc
+imap <M-c> <C-O>gc$
+" 
+inoremap <M-a> <C-O>a
+inoremap <M-A> <C-O>A
+
+" #tab
+nnoremap <M-CR> :tabnew<CR>
+nnoremap <M-TAB> :tabnext<CR>
+tnoremap <M-TAB> :tabnext<CR>
+nnoremap <M-Del> :tabclose<CR>
+
+
+map <M--> :wincmd <<CR>
+tmap <M--> <C-\><C-n>:wincmd <<CR>
+map <M-=> :wincmd ><CR>
+tmap <M-=> <C-\><C-n>:wincmd ><CR>
+
+map <M-h> :wincmd h<CR>
+tmap <M-h> <C-\><C-n>:wincmd h<CR>
+map <M-j> :wincmd j<CR>
+tmap <M-j> <C-\><C-n>:wincmd j<CR>
+map <M-k> :wincmd k<CR>
+tmap <M-k> <C-\><C-n>:wincmd k<CR>
+map <M-l> :wincmd l<CR>
+tmap <M-l> <C-\><C-n>:wincmd l<CR>
+
+" map <M-H> :wincmd H<CR>
+" map <M-J> :wincmd J<CR>
+" map <M-K> :wincmd K<CR>
+" map <M-L> :wincmd L<CR>
+
+nnoremap cp "+y
+vnoremap cp "+y
+
+" #space
+nnoremap <SPACE> <Nop>
+nnoremap <Space>m :messages<CR>
+map <silent> <TAB> :bn<CR>
+map <silent> <S-TAB> :bp<CR>
+
+map <silent> [q :cp<CR>
+map <silent> ]q :cn<CR>
+
+map <silent> <leader>. :vertical resize +5<CR>
+map <silent> <leader>, :vertical resize -5<CR>
+
+
+nmap <leader>q :quitall<CR>
+
+if !exists('*SaveExec')
+  function! SaveExec() abort
+    if &filetype == 'vim'
+      :silent! write
+      :source %
+    elseif &filetype == 'lua'
+      :silent! write
+      :luafile %
+    endif
+    return
+  endfunction
+endif
+nmap <leader><leader>a :call SaveExec()<CR>
+
+function! Ctoggle()
+  if empty(filter(getwininfo(), 'v:val.quickfix'))
+    copen
+  else
+    cclose
+  endif
+endfunction
+nnoremap tq :call Ctoggle()<CR>
+
---
+++ plugin/_lualine.lua
@@ -0,0 +1,97 @@
+--- @ https://github.com/nvim-lualine/lualine.nvim
+require('lualine').setup({
+  options = {
+    disabled_filetypes = {
+      statusline = { 'NvimTree', 'neotest-summary', 'neotest-output-panel' },
+      winbar = { 'NvimTree', 'neotest-summary', 'neotest-output-panel' },
+      'dapui_watches',
+      'dapui_breakpoints',
+      'dapui_scopes',
+      'dapui_console',
+      'dapui_stacks',
+      'dap-repl',
+    },
+  },
+  tabline = {
+    -- https://github.com/nvim-lualine/lualine.nvim#buffers-component-options
+    lualine_a = { 'tabs' },
+    lualine_b = {
+      {
+        'buffers',
+        show_filename_only = true,
+        max_length = vim.o.columns * 4 / 5,
+        mode = 4,
+      },
+    },
+    -- lualine_c = {},
+    lualine_x = {
+      -- {
+      --   'filename',
+      --   file_status = true, -- Displays file status (readonly status, modified status)
+      --   newfile_status = false, -- Display new file status (new file means no write after created)
+      --   path = 1, -- 0: Just the filename
+      --   -- 1: Relative path
+      --   -- 2: Absolute path
+      --   -- 3: Absolute path, with tilde as the home directory
+      --   -- 4: Filename and parent dir, with tilde as the home directory
+
+      --   shorting_target = 20, -- Shortens path to leave 40 spaces in the window
+      --   -- for other components. (terrible name, any suggestions?)
+      --   symbols = {
+      --     modified = '[+]', -- Text to show when the file is modified.
+      --     readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
+      --     unnamed = '[No Name]', -- Text to show for unnamed buffers.
+      --     newfile = '[New]', -- Text to show for newly created file before first write
+      --   },
+      -- },
+    },
+    lualine_y = {},
+    lualine_z = {
+      'searchcount',
+    },
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { vim.uv.cwd },
+    lualine_c = { { 'filename', path = 1 } },
+    lualine_x = {
+      {
+        'lsp_progress',
+        display_components = {
+          'lsp_client_name',
+          {
+            'percentage',
+          },
+        },
+      },
+      'encoding',
+      'fileformat',
+      'filetype',
+    },
+    lualine_y = { 'progress' },
+    lualine_z = {
+      function()
+        return vim.api.nvim_buf_line_count(0)
+      end,
+      'location',
+    },
+  },
+  inactive_sections = {
+    lualine_c = { 'filename' },
+    lualine_x = {
+      'location',
+      function()
+        return vim.api.nvim_buf_line_count(0)
+      end,
+    },
+  },
+  extensions = {
+    'quickfix',
+    'nvim-tree',
+    'fzf',
+    'symbols-outline',
+    'toggleterm',
+    'man',
+    'trouble',
+  },
+})
