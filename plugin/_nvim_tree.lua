f2e11c361be5ea501ca8f7b60bfc75b3274529a7
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2024-02-08 19:29:15 +0200

    nvimtree

---
+++ plugin/_nvim_tree.lua
@@ -0,0 +1,22 @@
+-- https://github.com/kyazdani42/nvim-tree.lua
+require('nvim-tree').setup({
+  on_attach = function(bufnr)
+    require('nvim-tree.api').config.mappings.default_on_attach(bufnr)
+    vim.keymap.set(
+      'n',
+      '<C-Space>',
+      tree_actions_menu,
+      { buffer = bufnr, noremap = true, silent = true }
+    )
+  end,
+  sync_root_with_cwd = true,
+  update_focused_file = {
+    enable = true,      -- default false
+    update_root = true, -- default false
+    debounce_delay = 15,
+    ignore_list = {},
+  },
+  git = { enable = false },
+  view = { width = 25, signcolumn = 'no' },
+  ui = { confirm = { trash = false } },
+})
