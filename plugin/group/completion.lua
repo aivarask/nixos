aa943720485bf50486588bcab99c8ebc0d428cb3
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2024-02-09 07:51:15 +0200

    some completion narrowing

--- plugin/group/completion.lua
+++ plugin/group/completion.lua
@@ -3,44 +3,22 @@ local cmp = require('cmp')
 -- https://github.com/L3MON4D3/LuaSnip
 local luasnip = require('luasnip')
 require('luasnip.loaders.from_vscode').lazy_load()
-require('luasnip.loaders.from_vscode').load({
-  paths = '/etc/nixos/snippets',
-})
+require('luasnip.loaders.from_vscode').load({ paths = '/etc/nixos/snippets' })
 
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 
 ---@type cmp.ConfigSchema
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
+  snippet = { expand = function(args) require('luasnip').lsp_expand(args.body) end },
   mapping = cmp.mapping.preset.insert({
     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({
-      select = false,
-      cmp.ConfirmBehavior.Replace,
-    }),
-
-    ['<Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-    ['<S-Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end,
+    ['<CR>'] = cmp.mapping.confirm({ select = false, cmp.ConfirmBehavior.Replace }),
+    ['<Tab>'] = function(fallback) if cmp.visible() then cmp.select_next_item() else fallback() end end,
+    ['<S-Tab>'] = function(fallback) if cmp.visible() then cmp.select_prev_item() else fallback() end end,
     ['<C-n>'] = cmp.mapping(function(fallback)
       if luasnip.jumpable(1) then
         luasnip.jump(1)
@@ -62,31 +40,20 @@ cmp.setup({
     { name = 'emoji' },
   }, {
     { name = 'path' },
-    -- { name = 'buffer' },
+    { name = 'buffer' },
   }),
 })
 
 cmp.setup.cmdline({ '/', '?' }, {
   mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' },
-  },
+  sources = { { name = 'buffer' } },
 })
 
 cmp.setup.cmdline(':', {
   mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' },
-  }, {
-    { name = 'cmdline' },
-  }),
+  sources = cmp.config.sources({ { name = 'path' }, { name = 'cmdline' } }),
 })
 
--- Set configuration for specific filetype.
--- cmp.setup.filetype('gitcommit', {
---   sources = cmp.config.sources({
---     { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
---   }, {
---     { name = 'buffer' },
---   }),
--- })
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({ { name = 'git' } }, { { name = 'buffer' } }),
+})
