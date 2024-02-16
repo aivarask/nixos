cefa1fffd65eea853d433d4804463acf7c982a94
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2024-02-16 09:40:06 +0200

    st config only

--- config/environment/suckless.nix
+++ config/environment/suckless.nix
@@ -16,7 +16,8 @@
       st = super.st.overrideAttrs (oldAttrs: rec {
         # https://st.suckless.org/patches/
         patches = [
-          ./st-fontsize.diff
+          # ./st-fontsize.diff
+          # ./st-delkey-20201112-4ef0cbd.diff
           # st -z 32 -e ...
         ];
         configFile = super.writeText "config.h" (builtins.readFile ./st-config.h);
