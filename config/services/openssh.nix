88ed206d9b1f8bb1a2b8b7a7df46eeb1a28fac1a
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2024-08-16 21:04:29 +0300

    openssh X11

--- config/services/openssh.nix
+++ config/services/openssh.nix
@@ -2,8 +2,12 @@
   # networking.firewall.allowedTCPPorts = [ 22 ];
   services.openssh = {
     enable = true;
-    settings.PermitRootLogin = "yes";
     openFirewall = true;
+    settings.PermitRootLogin = "yes";
+    settings = {
+      # https://unix.stackexchange.com/questions/12755/how-to-forward-x-over-ssh-to-run-graphics-applications-remotely
+      X11Forwarding = true;
+    };
   };
   # openssh.authorizedKeys
   # ../users.nix
