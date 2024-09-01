824ee683b9625d0d2702b46db3fced764c5c5cec
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2024-09-01 10:27:45 +0300

    add ssh id

--- config/users.nix
+++ config/users.nix
@@ -7,6 +7,7 @@
     extraGroups = [ "http" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHEs8Ir7meX21p/xxIfwz/Z9vYDF0VCE29t4pML7iF/X root@dell"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIANknZM5gmvqk6PlxOsiiPtmGEOH6HCIGs/YSarBXfEW root@pc"
     ];
   };
   users.users.ak = {
