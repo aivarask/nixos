608f00d8af4264bb5a29ca3e0f546bb2be622ffa
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2024-10-15 14:32:03 +0300

    nfu

--- lua/cfg/format.lua
+++ lua/cfg/format.lua
@@ -3,13 +3,13 @@ local format = vim.api.nvim_create_augroup('_format', {})
 vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
 	group = format,
 	pattern = {
-		'*.lua',
-		'*.html',
-		'*.xml',
-		'*.twig',
 		'*.nix',
+		'*.lua',
+		-- '*.html',
+		-- '*.xml',
+		-- '*.twig',
 	},
-	desc = 'vim.lsp.buf.format null-ls',
+	desc = 'vim.lsp.buf.format null-ls 1000',
 	callback = function()
 		vim.lsp.buf.format({
 			-- async = true,
@@ -19,11 +19,12 @@ vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
 	end,
 })
 
-vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
-	group = format,
-	pattern = { [[?*\(.vim\|.lua\|.nix\|sxhkdrc\)\@<!]] },
-	desc = 'vim.lsp.buf.format',
-	callback = function()
-		vim.lsp.buf.format({ timeout_ms = 1000 })
-	end,
-})
+-- vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+-- 	group = format,
+-- 	-- pattern = { [[?*\(.vim\|.lua\|.nix\|sxhkdrc\)\@<!]] },
+-- 	pattern = { [[?*\(.vim\|.lua\|.nix\|sxhkdrc\)\@<!]] },
+-- 	desc = 'vim.lsp.buf.format',
+-- 	callback = function()
+-- 		vim.lsp.buf.format({ timeout_ms = 1000 })
+-- 	end,
+-- })
