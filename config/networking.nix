b9cfad091e7cf0681d66d3754551b58324125dbe
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2024-11-04 07:58:59 +0200

    include rules in dns

--- config/networking.nix
+++ config/networking.nix
@@ -36,14 +36,9 @@
     '';
   };
   networking.hosts = {
-    "127.0.0.1" = [ "local.local" ];
     "192.168.1.1" = [
       "hw"
       "hw.local"
-    ];
-    "192.168.1.190" = [
-      "pcl"
-      "pcl.local"
     ];
     "192.168.1.195" = [
       "pcw"
@@ -53,18 +48,30 @@
       "dell"
       "dell.local"
     ];
-    "192.168.1.170" = [
-      "i7"
-      "i7.local"
-    ];
-    "192.168.1.150" = [
-      "a5"
-      "a5.local"
-    ];
-    "192.168.1.127" = [
-      "redmi"
-      "redmi.local"
-    ];
+  };
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      forwarding_rules = "/etc/nixos/config/networking.rules";
+      # https://github.com/DNSCrypt/dnscrypt-proxy/blob/master/dnscrypt-proxy/example-dnscrypt-proxy.toml
+      ipv6_servers = false;
+      require_dnssec = true;
+
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+
+      # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+      # server_names = [ ... ];
+    };
+  };
+  systemd.services.dnscrypt-proxy2.serviceConfig = {
+    StateDirectory = "dnscrypt-proxy";
   };
   networking.wireless = {
     enable = lib.mkDefault true;
