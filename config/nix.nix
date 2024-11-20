948573970924823763829acc9397a065559a5992
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2024-11-20 08:54:28 +0200

    cachix top

--- config/nix.nix
+++ config/nix.nix
@@ -28,8 +28,8 @@
       cores = 4;
       auto-optimise-store = false;
       substituters = [
-        "https://cache.nixos.org/"
         "https://hyprland.cachix.org"
+        "https://cache.nixos.org/"
       ];
       trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
     };
