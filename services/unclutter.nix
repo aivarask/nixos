34157adb50c1ccaf27d784a1f35d5a2985b0943c
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-01-11 10:43:18 +0200

    choose unclutter-xfixes

--- services/unclutter.nix
+++ services/unclutter.nix
@@ -1,22 +1,15 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = [
-    # pkgs.unclutter
-    # pkgs.xorg.libX11
-  ];
-  services.xbanish = {
+  # services.xbanish.enable = true;
+  services.unclutter-xfixes = {
+    # package = pkgs.unclutter-xfixes;
     enable = true;
-    arguments = "";
+    threshold = 1;
+    timeout = 1;
+    extraOptions = [
+      # "noevents"
+      # "grab"
+      # "fork"
+    ];
   };
-  # services.unclutter = {
-  #   package = pkgs.unclutter-xfixes;
-  #   enable = true;
-  #   threshold = 1;
-  #   timeout = 1;
-  #   extraOptions = [
-  #     # "noevents"
-  #     # "grab"
-  #     # "fork"
-  #   ];
-  # };
 }
