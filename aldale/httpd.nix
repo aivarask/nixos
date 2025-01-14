e9d8b49043a23e3cc97349a4a0e398482880b38f
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-01-14 17:46:20 +0200

    add flake

--- aldale/httpd.nix
+++ aldale/httpd.nix
@@ -1,43 +1,14 @@
-{ pkgs, config, ... }:
-let
-  phpCustom = pkgs.php.buildEnv {
-    extensions = (
-      { enabled, all }:
-      enabled
-      ++ (with all; [
-        xdebug
-      ])
-    );
-    extraConfig = ''
-      xdebug.idekey=neovim
-      xdebug.mode=debug
-      xdebug.start_with_request=yes
-      upload_max_filesize=30M
-      post_max_size=31M
-      memory_limit = 100M
-      max_execution_time = 120
-    '';
-    # zend_extension=${pkgs.phpExtensions.xdebug.outPath}/lib/php/extensions/xdebug.so
-  };
-in
+{ ... }:
 {
-  environment.systemPackages = [ phpCustom ];
-  networking.firewall.allowedTCPPorts = [
-    80
-    443
-  ];
   services.httpd = {
     enable = true;
     user = "root";
     group = "root";
     logPerVirtualHost = false;
-    logFormat = "common"; # combined, *common, referer, agent, none
-    enablePHP = true;
-    phpPackage = phpCustom;
+    logFormat = "common";
     extraModules = [
       "proxy_wstunnel"
     ];
     extraConfig = '''';
   };
-
 }
--- aldale/php.nix
+++ aldale/php.nix
@@ -1,4 +1,27 @@
 { pkgs, ... }:
+let
+  phpCustom = pkgs.php.buildEnv {
+    extensions = (
+      { enabled, all }:
+      enabled
+      ++ (with all; [
+        xdebug
+      ])
+    );
+    extraConfig = ''
+      xdebug.idekey=neovim
+      xdebug.mode=debug
+      xdebug.start_with_request=yes
+      upload_max_filesize=30M
+      post_max_size=31M
+      memory_limit = 100M
+      max_execution_time = 120
+    '';
+    # zend_extension=${pkgs.phpExtensions.xdebug.outPath}/lib/php/extensions/xdebug.so
+  };
+in
 {
-
+  environment.systemPackages = [ phpCustom ];
+  services.httpd.enablePHP = true;
+  services.httpd.phpPackage = phpCustom;
 }
