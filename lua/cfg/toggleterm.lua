347a50cad16573ff08b83291035032c215f44e9f
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-01-16 13:24:43 +0200

    synergy toggleterm

--- lua/cfg/toggleterm.lua
+++ lua/cfg/toggleterm.lua
@@ -27,6 +27,8 @@ lf = t:new({ cmd = 'lf', hidden = true, direction = 'float' })
 htop = t:new({ cmd = 'htop', hidden = true, direction = 'float' })
 nix_repl_self = t:new({ cmd = 'nix repl self', hidden = true, direction = 'float' })
 ncmpcpp = t:new({ cmd = 'ncmpcpp', hidden = true, direction = 'float' })
+sysz = t:new({ cmd = 'sysz', hidden = true, direction = 'tab' })
+sysz_u = t:new({ cmd = 'sysz -u', hidden = true, direction = 'tab' })
 
 local mode = { 'n', 't' }
 require('which-key').add({
@@ -35,4 +37,6 @@ require('which-key').add({
 	{ '<C-`>', '<cmd>ToggleTerm direction=tab<CR>', mode = { 'n', 't' } },
 	{ '<C-1>', function() nix_repl_self:toggle(nil, 'tab') end, mode = mode },
 	{ '<C-2>', function() lf:toggle(nil, 'float') end, mode = mode },
+	{ '<C-3>', function() sysz:toggle(nil, 'tab') end, mode = mode },
+	{ '<C-4>', function() sysz_u:toggle(nil, 'tab') end, mode = mode },
 })
