f7dc3fc0678d0f26db71b10c0d5cc2a0946c6448
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-01-19 09:59:09 +0200

    lua flake

--- lua/vim_.nix
+++ lua/vim_.nix
@@ -154,10 +154,10 @@ in
     enable = true;
     extraConfig = ''
       let &packpath.=',/etc/nixos'
-      let &runtimepath.=',/etc/nixos,/etc/nixos/awe,/etc/nixos/luvit'
+      let &runtimepath.=',/etc/nixos,/etc/nixos/awe'
       runtime! lua/cfg/**/*{.lua,.vim}
       runtime! lua/_*{.lua,.vim}
-      runtime! {deps,lsp }/**/*.lua
+      runtime! lsp/**/*.lua
 
       set scrolloff=12
       set cmdheight=1
