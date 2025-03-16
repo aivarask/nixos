cfe38d083d611c71ddf5ca0e73e8dbf8a65fe7af
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-03-16 08:10:57 +0200

    openssh

--- services/openssh.nix
+++ services/openssh.nix
@@ -1,9 +1,18 @@
 { ... }:
 {
+  # https://www.xmodulo.com/how-to-speed-up-x11-forwarding-in-ssh.html
+  # https://mynixos.com/nixpkgs/option/services.openssh.settings.Ciphers
+
+  programs.ssh = {
+    # ciphers = [ ];
+    forwardX11 = true;
+    # compression = true; # home-manager
+  };
   services.openssh = {
     enable = true;
     openFirewall = true;
     settings = {
+      # Ciphers = [ ];
       X11Forwarding = true;
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
