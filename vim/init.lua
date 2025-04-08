4dcc647b913f7691944911601188e30f207ebcb2
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-04-08 23:59:19 +0300

    vim

--- vim/init.lua
+++ vim/init.lua
@@ -1,2 +1,9 @@
-vim.cmd [[ source $XDG_CONFIG_HOME/nvim/nvimrc.vim ]]
+vim.cmd [[
+"source $XDG_CONFIG_HOME/nvim/nvimrc.vim
+let &runtimepath.=',/etc/nixos'
+let &packpath.=',/etc/nixos'
+runtime! lua/cfg/**/*{.lua,.vim}
+runtime! lua/_*{.lua,.vim}
+]]
 vim.loader.enable()
+require('help')
--- vim/nvimrc.vim
+++
@@ -1,5 +0,0 @@
-let &packpath.=',/etc/nixos'
-let &runtimepath.=',/etc/nixos'
-runtime! lua/cfg/**/*{.lua,.vim}
-runtime! lua/_*{.lua,.vim}
-
