07ede19dd8d1ee8103e2293fb34a0504ce248cba
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-04-08 17:58:09 +0300

    mpv keys

--- mpv/default.nix
+++ mpv/default.nix
@@ -11,7 +11,10 @@
       mpv = pkgs.mpv-unwrapped.override {
         # waylandSupport = true;
       };
-      scripts = with pkgs.mpvScripts; [ pkgs.mpvScripts.uosc ];
+      scripts = with pkgs.mpvScripts; [
+        pkgs.mpvScripts.uosc # https://github.com/tomasklaen/uosc
+        pkgs.mpvScripts.reload # https://github.com/4e6/mpv-reload
+      ];
     }
   );
   # MPV_HOME="/etc/nixos/mpv";
