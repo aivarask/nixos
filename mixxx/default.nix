961d2e3fc5c3f1e8ad4ddb5d4d1872069b75a420
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-06-07 17:44:30 +0300

    mixxx controller

--- mixxx/default.nix
+++ mixxx/default.nix
@@ -1,5 +1,7 @@
 { config, ... }:
 {
-  home.file.".mixxx/controllers/_CMD".source =
-    config.lib.file.mkOutOfStoreSymlink "/etc/nixos/mixxx/Behringer-CMD-MM1-scripts.js";
+  home.file.".mixxx/controllers/_Behringer-CMD-MM1-scripts.js".source =
+    config.lib.file.mkOutOfStoreSymlink "/etc/nixos/mixxx/_Behringer-CMD-MM1-scripts.js";
+  home.file.".mixxx/controllers/_Behringer CMD MM-1.midi.xml".source =
+    config.lib.file.mkOutOfStoreSymlink "/etc/nixos/mixxx/_Behringer CMD MM-1.midi.xml";
 }
