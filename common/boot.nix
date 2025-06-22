3b6141ca1d4753bac3c39aa3ca17b72b63fc7e3b
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-06-22 18:27:17 +0300

    add netboot

--- common/boot.nix
+++ common/boot.nix
@@ -4,6 +4,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
   boot.loader.systemd-boot.enable = lib.mkDefault true;
+  boot.loader.systemd-boot.netbootxyz.enable = true;
   boot.kernel.sysctl = {
     "fs.inotify.max_user_watches" = 100001;
     "fs.inotify.max_queued_events" = 100001;
