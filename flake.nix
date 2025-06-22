bdcad527ad40aeeee1e03ab7d2fd9cb7d923285c
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-06-22 19:09:03 +0300

    minimal_iso

--- flake.nix
+++ flake.nix
@@ -184,6 +184,30 @@
       # packages./*<SYSTEM>.*/"<PACKAGE>" = /* ... */;
     })
     // inputs.flake-utils.lib.eachDefaultSystemPassThrough (system: {
+      packages."${system}" = {
+        minimal_iso = inputs.nixos-generators.nixosGenerate {
+          inherit system;
+          modules = [
+            ./minimal.nix
+          ];
+          format = "install-iso";
+
+          # optional arguments:
+          # explicit nixpkgs and lib:
+          # pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          # lib = nixpkgs.legacyPackages.x86_64-linux.lib;
+          # additional arguments to pass to modules:
+          # specialArgs = { myExtraArg = "foobar"; };
+
+          # you can also define your own custom formats
+          # customFormats = { "myFormat" = <myFormatModule>; ... };
+          # format = "myFormat";
+        };
+        # vbox = inputs.nixos-generators.nixosGenerate {
+        #   system = "x86_64-linux";
+        #   format = "virtualbox";
+        # };
+      };
 
       nixosConfigurations.dell = nixpkgs.lib.nixosSystem {
         modules = commonModules ++ [ ./dell ];
