848c8651d92b4171f0f23784a6b0450f4c53a0e1
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-07-03 23:02:59 +0300

    nicotine

---
+++ audio/nicotine.nix
@@ -0,0 +1,19 @@
+{ pkgs, config, ... }:
+{
+  home.packages = with pkgs; [
+    nicotine-plus
+    slskd
+  ];
+  xdg.configFile."nicotine/config_def".source =
+    config.lib.file.mkOutOfStoreSymlink "/etc/nixos/audio/nicotine";
+  # xdg.configFile = {
+  #   "nicotine/config_def" = {
+  #     source = ./nicotine;
+  #     enable = true;
+  #     recursive = true;
+  #     onChange = ''
+  #       cat ${config.home.homeDirectory}/.config/nicotine/config_def > ${config.home.homeDirectory}/.config/nicotine/config
+  #     '';
+  #   };
+  # };
+}
