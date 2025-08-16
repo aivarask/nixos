7d79395341820582689f1c52498cf44af6c1261a
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-08-16 19:25:06 +0300

    ncmpcpp

--- ncmpcpp/default.nix
+++ ncmpcpp/default.nix
@@ -1,9 +1,14 @@
 { config, ... }:
 {
   programs.ncmpcpp.enable = true;
-  xdg.configFile."ncmpcpp" = {
-    enable = false;
+  xdg.configFile."ncmpcpp/config" = {
+    enable = true;
     force = true;
-    source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/ncmpcpp";
+    source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/ncmpcpp/config";
+  };
+  xdg.configFile."ncmpcpp/bindings" = {
+    enable = true;
+    force = true;
+    source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/ncmpcpp/bindings";
   };
 }
