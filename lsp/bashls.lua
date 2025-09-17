01c1314b0fc93228850260430bf19c87d19d0e92
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-09-17 12:51:44 +0300

    lsp bashls

--- lsp/bashls.lua
+++ lsp/bashls.lua
@@ -2,10 +2,4 @@ return {
 	-- name = "BASHLS",
 	filetypes = { 'sh', 'bash', 'zsh' },
 	cmd = { 'bash-language-server', 'start' },
-	-- settings = {
-	-- 	bashIde = {
-	-- 		globPattern = '*@(.sh|.inc|.bash|.command|.zsh)',
-	-- 		-- shellcheckArguments = { '--shell=bash' },
-	-- 	},
-	-- },
 }
