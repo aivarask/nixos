eb8c8c808ce997a1813fa32728033796687bd6e8
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-10-26 03:21:04 +0200

    spotify

---
+++ spotify.nix
@@ -0,0 +1,39 @@
+{
+  system =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = with pkgs; [
+        ncspot
+        spotify
+        spotifyd
+        spotify-qt
+
+      ];
+
+      # https://docs.spotifyd.rs
+      # https://github.com/Spotifyd/spotifyd/issues
+      services.spotifyd.enable = true;
+      services.spotifyd.settings = {
+        global = {
+          backend = "pipe";
+          device_type = "computer";
+          use_mpris = false;
+          # bitrate = 320;
+          dbus_type = "system";
+        };
+      };
+      # services.playerctld.enable = true;
+
+      systemd.tmpfiles.settings."10-spotifyd" = {
+        "/root/.config/spotifyd/spotifyd.conf" = {
+          "L+" = {
+            user = "root";
+            group = "root";
+            mode = "0777";
+            type = "L+";
+            argument = "/etc/nixos/spotifyd.conf";
+          };
+        };
+      };
+    };
+}
