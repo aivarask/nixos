326a9fc90ec4b9f03a5248b4385238ea29a188a9
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-10-26 03:23:38 +0200

    llm

--- flake.nix
+++ flake.nix
@@ -105,40 +105,13 @@
         ./systemd/video.nix
         ./steam
 
-        (
-          # lmms
-          { pkgs, ... }:
-          {
-            # https://www.youtube.com/watch?v=GuTcle5edjk
-            environment.systemPackages = with pkgs; [
-              obsidian
-              litellm
-              claude-code
-              lmstudio
-              open-webui
-              n8n
+        (import ./llm.nix).system
 
-              python3Packages.huggingface-hub
-              # python3Packages.transformers
-              # python3Packages.trlib
-              cudatoolkit
-              # python3Packages.llama-index-cli
-              # python3Packages.llama-index-llms-ollama
-            ];
-            services.ollama.enable = true;
-            services.ollama.package = pkgs.ollama-cuda;
-            services.ollama.environmentVariables.OLLAMA_LLM_LIBRARY = "cuda";
-            services.ollama.environmentVariables.LD_LIBRARY_PATH = "run/opengl-driver/lib";
-            services.ollama.acceleration = "cuda"; # long build
-            services.ollama.loadModels = [ ];
-          }
-        )
-
-        (import ./spotify.nix).system
         (import ./users/aiva.nix).system
         ./touchegg
         ./virt/virtualbox.nix
         ./wallpaper
+        (import ./spotify.nix).system
         (import ./sh/env.nix).system
         (import ./x11).default
         (import ./yt-dlp).system
