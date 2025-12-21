9d8a7f929b7b74d384fc57f9c7e15c33e9ffb097
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-12-21 16:24:45 +0200

    ff

--- hosts/dell/storage.nix
+++ hosts/dell/storage.nix
@@ -16,13 +16,13 @@
   };
   swapDevices = [
   ];
-  fileSystems."/mnt/pc_music" = {
-    device = "//192.168.1.185/music";
+  fileSystems."/mnt/Shares" = {
+    device = "//192.168.1.185/mnt/Shares";
     fsType = "cifs";
     options =
       let
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
       in
-      [ "${automount_opts},credentials=/etc/nixos/dell/smb-secrets" ];
+      [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
   };
 }
--- hosts/pc/storage.nix
+++ hosts/pc/storage.nix
@@ -14,14 +14,13 @@
     ];
   };
   swapDevices = [ ];
-  fileSystems."/mnt/dell_music" = {
-    device = "//192.168.1.180/music";
+  fileSystems."/mnt/Shares" = {
+    device = "//192.168.1.180/mnt/Shares";
     fsType = "cifs";
-    options = [
-      "username=root"
-      "users"
-      "noauto"
-      "credentials=/etc/nixos/smb-secrets"
-    ];
+    options =
+      let
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      in
+      [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
   };
 }
