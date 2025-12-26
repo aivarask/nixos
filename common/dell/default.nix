153ca5db113153c0ad3f504bfa783840640a4d82
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2025-12-26 05:19:45 +0200

    sway

--- common/dell/default.nix
+++ common/dell/default.nix
@@ -69,9 +69,9 @@
   # hardware.nvidia.prime.allowExternalGpu = true;
   # hardware.nvidia.prime.reverseSync.enable = true;
 
-  hardware.nvidia.modesetting.enable = true;
+  # hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
   boot.kernelParams = [
-    "module_blacklist=i915"
+    # "module_blacklist=i915"
   ];
 }
