2114901eb4160896183a279ddead6a8b0d08f3e3
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2026-01-08 09:46:21 +0200

    zsh

--- .programs.nix
+++ .programs.nix
@@ -38,57 +38,7 @@ in
         };
       }
     )
-    (
-      { pkgs, ... }:
-      {
-        # https://github.com/rothgar/mastering-zsh
-        home.packages = with pkgs; [
-          bash-language-server
-          shellcheck
-          shfmt
-          dotenv-linter
-          shellharden
-        ];
-        xdg.configFile."zsh/.zshenv_".source = symlink "${xdgconf}/zsh/.zshenv_";
-        xdg.configFile."zsh/.zshrc_".source = symlink "${xdgconf}/zsh/.zshrc_";
-        programs.zsh.envExtra = builtins.concatStringsSep "\n" [
-          "source $ZDOTDIR/.zshenv_"
-        ];
-        programs.zsh.initContent = builtins.concatStringsSep "\n" [
-          "source $ZDOTDIR/.zshrc_"
-        ];
-        programs.zsh.enable = true;
-        programs.zsh.autocd = true;
-        programs.zsh.syntaxHighlighting.enable = true;
-        programs.zsh.autosuggestion.enable = false;
-        programs.zsh.dotDir = "${config.xdg.configHome}/zsh";
-        programs.zsh.history.size = 10000;
-        programs.zsh.history.extended = true;
-        programs.zsh.plugins = [
-          {
-            name = "zsh-zhooks"; # https://github.com/agkozak/zhooks
-            src = pkgs.zsh-zhooks;
-            file = "share/zsh/zhooks/zhooks.plugin.zsh";
-          }
-          {
-            name = "zsh-autocomplete"; # https://github.com/marlonrichert/zsh-autocomplete
-            src = pkgs.zsh-autocomplete;
-            file = "share/zsh-autocomplete/zsh-autocomplete.plugin.zsh";
-          }
-          {
-            name = "vi-mode";
-            src = pkgs.zsh-vi-mode; # https://github.com/jeffreytse/zsh-vi-mode
-            file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
-          }
-          {
-            name = "system-clipboard";
-            src = pkgs.zsh-system-clipboard; # https://github.com/kutsan/zsh-system-clipboard
-            file = "share/zsh/zsh-system-clipboard/zsh-system-clipboard.zsh";
-          }
-        ];
-
-      }
-    )
+    ./.config/zsh/default.nix
   ];
 
   home.packages = with pkgs; [
