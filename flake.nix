b1bd853fc1d21a74cad40239689c44a68a4f944d
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2026-01-24 17:30:23 +0200

    ff

--- flake.nix
+++ flake.nix
@@ -198,17 +198,17 @@
               system.stateVersion = "26.05";
               nixpkgs.hostPlatform = system; # lib.mkDefault "x86_64-linux";
               hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+              fileSystems."/" = {
+                device = "/dev/disk/by-partlabel/disk-main-root";
+                fsType = "ext4";
+              };
               fileSystems."/boot" = {
-                device = "/dev/disk/by-partlabel/disk-main-boot";
+                device = "/dev/disk/by-partlabel/disk-main-ESP";
                 fsType = "vfat";
                 options = [
                   "fmask=0077"
                   "dmask=0077"
                 ];
-              };
-              fileSystems."/" = {
-                device = "/dev/disk/by-partlabel/disk-main-root";
-                fsType = "ext4";
               };
 
             }
