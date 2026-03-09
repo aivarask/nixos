b45b7767149ea3ff50c8006818c23370337e91f0
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2026-03-09 20:50:57 +0200

    ff

--- graphics.nix
+++ graphics.nix
@@ -20,11 +20,11 @@
   ];
   hardware.graphics.extraPackages = [
     pkgs.intel-compute-runtime-legacy1
-    pkgs.intel-ocl
+    # pkgs.intel-ocl
   ];
-  hardware.nvidia.open = false;
+  hardware.nvidia.open = true;
   # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
   hardware.nvidia.prime.intelBusId = lib.mkDefault "PCI:0:2:0";
   hardware.nvidia.prime.nvidiaBusId = lib.mkDefault "PCI:1:0:0";
   hardware.nvidia.powerManagement.enable = lib.mkDefault true;
---
+++ graphics2.nix
@@ -0,0 +1,54 @@
+{ pkgs, lib, ... }:
+{
+  # This *should* be unnecessary, but some DE/WM modules
+  # forget to set it, and that makes people think they need
+  # `hardware.graphics.enable`.
+  #
+  # So just to be safe, since you'll blame the rest of the config
+  # for failure otherwise...
+  services.graphical-desktop.enable = lib.mkDefault true;
+
+  ######################
+  # Video acceleration #
+  ######################
+  environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
+
+  hardware.graphics.extraPackages = with pkgs; [
+    # Yes, you only need this, I don't know why the internet
+    # insists on adding all those other packages here, they
+    # either do nothing or they actively break vaapi
+    intel-media-driver
+  ];
+
+  ##################
+  #  Using nvidia  #
+  ##################
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    open = true;
+
+    powerManagement = {
+      enable = true;
+      finegrained = true;
+    };
+
+    # Note that these settings don't do *anything* if you're using wayland.
+    # If you *are* using wayland you can just delete all of this; whatever
+    # your primary GPU is will always be your primary GPU, assuming
+    # your firmware doesn't change its mind about which GPU to default
+    # to - if you want control over that, see my answer to this other post:
+    #
+    # https://discourse.nixos.org/t/why-nixos-using-dgpu-instead-of-igpu/73973/2?u=tlater
+    prime = {
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+    };
+  };
+}
