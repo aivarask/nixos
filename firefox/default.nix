86bff35b0c9d646061158a5bfef2994f2a3f2851
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2026-04-05 05:13:37 +0300

    dell b

--- firefox/default.nix
+++ firefox/default.nix
@@ -22,7 +22,7 @@ in
   programs.firefox.languagePacks = [ "en-US" ];
   xdg.configFile."${d}/default.nix".source = symlink "${s}/default.nix";
   xdg.configFile."${p}/chrome/userChrome.css".source = symlink "${s}/userChrome.css";
-  xdg.configFile."${p}/bookmarks.html".source = symlink "${s}/bookmarks.html";
+  # xdg.configFile."${p}/bookmarks.html".source = symlink "${s}/bookmarks.html";
   # xdg.configFile."${p}/prefs.js".source = symlink "${s}/prefs.js";
   xdg.configFile."${p}/user.js".source = symlink "${s}/user.js";
   xdg.configFile."${d}/profiles.ini".source = symlink "${s}/profiles.ini";
