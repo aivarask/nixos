8f21d95684ad1cc1563ed6580eafa4be77aea6b7
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2026-04-12 06:10:47 +0300

    dis

--- flake.nix
+++ flake.nix
@@ -95,7 +95,7 @@
           ./minimal.nix
           ./network.nix
           ./dell.nix
-          ./hdmi-cec.nix
+          # ./hdmi-cec.nix
         ];
       };
       nixosConfigurations.pc = inputs.nixpkgs.lib.nixosSystem {
