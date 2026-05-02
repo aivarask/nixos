7f7e53a75319b5a84bd379bb844f10f79aa0ce29
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2026-05-02 15:05:56 +0300

    dell adj

--- modules/searx.nix
+++ modules/searx.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }:
 {
-  services.searx.enable = true;
-  services.searx.redisCreateLocally = true;
+  services.searx.enable = false;
+  services.searx.redisCreateLocally = false;
   services.searx.settings.server = {
     bind_address = "::1";
-      # port = yourPort;
-      # WARNING: setting secret_key here might expose it to the nix cache
-      # see below for the sops or environment file instructions to prevent this
-      # secret_key = "Your secret key.";
-    };
-  }
+    # port = yourPort;
+    # WARNING: setting secret_key here might expose it to the nix cache
+    # see below for the sops or environment file instructions to prevent this
+    # secret_key = "Your secret key.";
+  };
+}
