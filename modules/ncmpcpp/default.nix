8d014db4311bac3d397d6abf08f5a9a02ac1e0f0
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2026-07-18 01:18:05 +0300

    ncmpcpp

--- modules/ncmpcpp/default.nix
+++ modules/ncmpcpp/default.nix
@@ -3,20 +3,18 @@
   home-manager.sharedModules = [
     (
       {
-        pkgs,
-        xdgconf,
         config,
         ...
       }:
       {
         programs.ncmpcpp.enable = true;
         xdg.configFile."ncmpcpp/config" = {
-          source = config.lib.file.mkOutOfStoreSymlink "${xdgconf}/ncmpcpp/config";
+          source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/modules/ncmpcpp/config";
           enable = true;
           force = true;
         };
         xdg.configFile."ncmpcpp/bindings" = {
-          source = config.lib.file.mkOutOfStoreSymlink "${xdgconf}/ncmpcpp/bindings";
+          source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/modules/ncmpcpp/bindings";
           enable = true;
           force = true;
         };
