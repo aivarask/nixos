71074f23a6568517c6f5757e4cb31820630e68dd
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2026-07-18 00:56:57 +0300

    http

--- httpd/branch.php
+++
@@ -1,5 +0,0 @@
-<?php
-$BRANCH = array('www', 'demo', 'live');
-foreach ($BRANCH as $val): ?>
-	<a href="?<?= http_build_query(array_merge($_GET, array('b' => $val))) ?>"><?= $val ?></a>
-<?php endforeach;
--- httpd/user/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db = new SQLite3('test.db');
-$db->exec("CREATE TABLE IF NOT EXISTS cars (id INTEGER PRIMARY KEY, name TEXT, price INT)");
-$db->exec("INSERT INTO cars (name, price) VALUES ('Audi', 52642)");
-$result = $db->query("SELECT * FROM cars");
-while ($row = $result->fetchArray()) {
-	echo "Car: " . $row['name'] . ", Price: " . $row['price'] . "\n";
-}
